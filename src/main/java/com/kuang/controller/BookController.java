08aa46c3790e8bf2862114341c8747fbfee5c30e
琬姗 曹 <634068240@qq.com>
2022-05-14 23:06:22 +0800

    添加书籍

--- src/main/java/com/kuang/controller/BookController.java
+++ src/main/java/com/kuang/controller/BookController.java
@@ -12,16 +12,30 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/book")
-public class BookController {
-// controller调serviceice层
+public class BookController {   // controller调service层
     @Autowired
     @Qualifier("BookServiceImpl")
     private BookService bookService;
+
 // 查询全部书籍，展示书籍
     @RequestMapping("/allBook")
     public String list(Model model) {
         List<Books> list = bookService.queryAllBook();
         model.addAttribute("list", list);
         return "allBook";
+    }
+// 跳转到增加书籍页面
+    @RequestMapping("/toAddBook")
+    public String toAddPaper() {
+        return "addBook";
+    }
+
+//    添加书籍
+    @RequestMapping("/addBook")
+    public String addPaper(Books books) {
+        System.out.println(books);
+        bookService.addBook(books);
+        // 重定向
+        return "redirect:/book/allBook";
     }
 }
